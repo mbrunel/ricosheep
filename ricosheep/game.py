13198f0c52d1044c404e4920df9d02a90a9ebdc7
Matthias Brunel <noreply@github.com>
2022-04-07 11:43:41 +0200

    constant sheep pathing

--- ricosheep/game.py
+++ ricosheep/game.py
@@ -1,5 +1,46 @@
-moves = {"left": (-1, 0), "up": (0, -1), "right": (1, 0), "down": (0, 1)}
-sorts = {"left": lambda x: x[0], "up": lambda x: x[1], "right": lambda x: -x[0], "down": lambda x: -x[1]}
+from operator import ne
+
+class Dot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __add__(self, o):
+        return Dot(self.x + o.x, self.y + o.y)
+    
+    def __sub__(self, o):
+        return Dot(self.x - o.x, self.y - o.y)
+    
+    def __iadd__(self, o):
+        self.x += o.x
+        self.y += o.y
+        return self
+    
+    def __isub__(self, o):
+        self.x -= o.x
+        self.y -= o.y
+        return self
+    
+    def __mul__(self, scal):
+        return Dot(self.x * scal, self.y * scal)
+    
+    def __imul__(self, scal):
+        self.x *= scal
+        self.y *= scal
+        return self
+    
+    def __eq__(self, o):
+        return self.x == o.x and self.y == o.y
+    
+    def set(self, new):
+        self.x = new.x
+        self.y = new.y
+    
+    def __hash__(self):
+        return (self.x, self.y).__hash__()
+
+
+moves = {"left": Dot(-1, 0), "up": Dot(0, -1), "right": Dot(1, 0), "down": Dot(0, 1)}
 
 class Cell:
     def __init__(self, v):
@@ -18,10 +59,11 @@ class Game:
     def compute_bushes(self):
         for y in range(len(self.board)):
             for x in range(len(self.board[y])):
-                for direction in moves:
-                    while 0 < x < self.width and 0 < y < self.height:
-                        if self.board[y][x].is_bush():
-                            self.board[y][x].nearest_bushes[direction] = (x, y)
+                for direction, move in moves.items():
+                    cell = Dot(x, y)
+                    while -1 < cell.x < self.width and -1 < cell.y < self.height and not self.board[cell.y][cell.x].is_bush():
+                        cell += move
+                    self.board[y][x].nearest_bushes[direction] = cell
 
     def load(self, filename):
         file = open(filename)
@@ -34,11 +76,11 @@ class Game:
                 row = []
                 continue
             if c == "S":
-                self.sheeps.append([len(row), len(self.board)])
+                self.sheeps.append(Dot(len(row), len(self.board)))
             if c == "S" or c == "_":
                 row.append(Cell(None))
             else:
-                row.append(Cell(None))
+                row.append(Cell(c))
         if len(row):
             self.board.append(row)
         self.height = len(self.board)
@@ -50,30 +92,35 @@ class Game:
     
     def play(self, direction):
         move = moves[direction]
-        self.sheeps.sort(key=sorts[direction])
         cache = {}
         for sheep in self.sheeps:
-             nearest_bush = self.board[sheep[0]][sheep[1]].nearest_bushes[direction]
-             if nearest_bush in cache:
+            nearest_bush = self.board[sheep.y][sheep.x].nearest_bushes[direction]
+            if nearest_bush not in cache:
+                cache[nearest_bush] = 0
+            cache[nearest_bush] += 1
+            sheep.set(nearest_bush - move * cache[nearest_bush])
+
     def isWon(self):
-        for x, y in self.sheeps:
-            if self.board[y][x] != 'G':
+        for sheep in self.sheeps:
+            if not self.board[sheep.y][sheep.x].is_grass():
                 return False
         return True
     
     def printBoard(self):
-        c = ''
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
-                if [j, i] in self.sheeps:
-                    if self.board[i][j] == 'G':
-                        c = 'H'
-                    else:
-                        c = 'S'
-                elif self.board[i][j] == None:
+                c = None
+                for sheep in self.sheeps:
+                    if sheep.x == j and sheep.y == i:
+                        if self.board[i][j].is_grass():
+                            c = 'H'
+                        else:
+                            c = 'S'
+                        break
+                if not c and not self.board[i][j].v:
                     c = '_'
-                else:
-                    c = self.board[i][j]
+                elif not c:
+                    c = self.board[i][j].v
                 print(c, end=' ')
             print()
 
